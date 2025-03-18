086b6513394de53769a31bfeece6dc946676a0c3
kingjenathe7th <jenakumoemmanuel@gmail.com>
2025-03-18 14:18:12 +0100

    feat: enhance DashboardBody and Slider components; improve layout, add headings and images to slides

--- src/dashboard/DashboardBody.jsx
+++ src/dashboard/DashboardBody.jsx
@@ -3,19 +3,19 @@ import { Slider } from "../components";
 
 const DashboardBody = () => {
   return (
-    <div className=" pt-18 h-full">
+    <div className=" pt-18 lg:pt-14 h-full">
       <div className="lg:pl-16 flex flex-col gap-6 ">
         <div className="py-4 border-b border-b-yellow-color bg-dashboard-header-bg">
           <div className="px-4 w-full max-w-7xl">
-            <div className="flex flex-col items-stretch">
+            <div className="flex flex-col lg:flex-row items-stretch lg:items-center">
               <div className="flex items-center gap-4 flex-[1_1_auto]">
                 <PiUserCircleDashed size={50} />
                 <span className="text-base leading-6 ">
                   Hello. Fill in your account details to make your first deposit
                 </span>
               </div>
-              <div className="flex gap-4 mt-6">
-                <button className="flex-1 inline-flex items-center justify-center cursor-pointer bg-light-gray px-4 py-2 rounded-md text-black min-w-20 min-h-10">
+              <div className="flex gap-4 mt-6 lg:mt-0">
+                <button className="flex-1 lg:flex-none inline-flex items-center justify-center cursor-pointer bg-light-gray px-4 py-2 rounded-md text-black min-w-20 min-h-10">
                   Learn more
                 </button>
 
--- src/dashboard/Slider.jsx
+++ src/dashboard/Slider.jsx
@@ -2,27 +2,34 @@ import { useState } from "react";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Slider() {
-  // Define three slides with unique id, background color, and text
+  // Define slides with unique id, optional background color, heading, text, and image URL
   const slides = [
     {
       id: 0,
       color: "bg-slidesbg",
       heading: "1000+ trading strategies",
-      text: "Slide 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      text: "Availiable for you to invest in",
+      image:
+        "https://my.ex-markets.pro/download/3d/3d461/3d461af64253a12a718b447b76a96ade.webp",
     },
     {
       id: 1,
-      color: "",
-      text: "Slide 2: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      color: "bg-slidesbg",
+      heading: "Secure your Funds",
+      text: "With push notifications to confirm withdrawals",
+      image:
+        "https://my.ex-markets.pro/download/e8/e82ca/e82ca62e19c1241bb9224e4b3756eeae.webp",
     },
     {
       id: 2,
-      color: "",
-      text: "Slide 3: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+      color: "bg-slidesbg",
+      heading: "Become a partner",
+      text: "invite a friend and earn up to 40% of their income",
+      image:
+        "https://my.ex-markets.pro/download/29/29864/29864a05ecae050738523b9e1f0fcbd2.webp",
     },
   ];
 
-  // Current slide index
   const [activeIndex, setActiveIndex] = useState(0);
 
   // Move to the next slide (with wrap-around)
@@ -38,64 +45,66 @@ export default function Slider() {
   };
 
   return (
-    <div className=" w-full max-w-xl mx-auto px-6 rounded-lg lg:hidden relative">
-      {/* Slider Container */}
-      <div className=" overflow-hidden ">
-        {/* Slides Wrapper: shifting slides based on activeIndex */}
-        <div
-          className="flex transition-transform duration-500"
-          style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-        >
-          {slides.map((slide) => (
-            <div
-              key={slide.id}
-              className={`${slide.color} flex-shrink-0 w-full h-30 flex items-center justify-center rounded-lg p-2`}
-            >
-              <div>
-                <h2 className="text-black text-base text-left font-semibold">
-                  {slide.heading}
-                </h2>
-                <p className="text-white text-sm ">{slide.text}</p>
-              </div>
+    <>
+      <div className="w-full px-6 rounded-lg  relative lg:hidden">
+        {/* Slider Container */}
+        <div className="overflow-hidden">
+          {/* Slides Wrapper: shifting slides based on activeIndex */}
+          <div
+            className="flex transition-transform duration-500"
+            style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+          >
+            {slides.map((slide) => (
               <div
-                style={{
-                  backgroundImage: `url(https://my.ex-markets.pro/download/e8/e82ca/e82ca62e19c1241bb9224e4b3756eeae.webp)`,
-                }}
-                className="bg-cover bg-center bg-no-repeat  min-w-[100px] min-h-[100px]"
-              ></div>
-            </div>
-          ))}
+                key={slide.id}
+                className={`${slide.color} flex-shrink-0 w-full h-28 flex items-center justify-between rounded-lg p-3.5`}
+              >
+                <div>
+                  {slide.heading && (
+                    <h2 className="text-black text-base text-left font-semibold">
+                      {slide.heading}
+                    </h2>
+                  )}
+                  <p className="text-black text-sm">{slide.text}</p>
+                </div>
+                <div
+                  style={{ backgroundImage: `url(${slide.image})` }}
+                  className="bg-cover bg-center bg-no-repeat min-w-[100px] min-h-[100px]"
+                ></div>
+              </div>
+            ))}
+          </div>
+
+          {/* Prev Button */}
+          <button
+            onClick={prevSlide}
+            className="absolute top-1/3 left-0 transform -translate-y-1/2 bg-gray-200 bg-opacity-75 hover:bg-opacity-100 text-gray-500 font-bold p-2.5 rounded-full inline-flex items-center cursor-pointer backdrop-blur-sm"
+          >
+            <MdKeyboardArrowLeft />
+          </button>
+
+          {/* Next Button */}
+          <button
+            onClick={nextSlide}
+            className="absolute top-1/3 right-0 transform -translate-y-1/2 bg-gray-300 bg-opacity-75 hover:bg-opacity-100 text-gray-500 font-bold p-2.5 rounded-full inline-flex items-center cursor-pointer backdrop-blur-sm"
+          >
+            <MdKeyboardArrowRight />
+          </button>
         </div>
 
-        {/* Prev Button */}
-        <button
-          onClick={prevSlide}
-          className="absolute top-1/3 left-0 transform -translate-y-1/2 bg-gray-200 bg-opacity-75 hover:bg-opacity-100 text-gray-500 font-bold p-2.5 rounded-full inline-flex items-center cursor-pointer backdrop-blur-sm"
-        >
-          <MdKeyboardArrowLeft />
-        </button>
-
-        {/* Next Button */}
-        <button
-          onClick={nextSlide}
-          className="absolute top-1/3 right-0 transform -translate-y-1/2 bg-gray-300 bg-opacity-75 hover:bg-opacity-100 text-gray-500 font-bold p-2.5 rounded-full inline-flex items-center cursor-pointer backdrop-blur-sm"
-        >
-          <MdKeyboardArrowRight />
-        </button>
+        {/* Indicator Buttons */}
+        <div className="flex justify-center mt-3 space-x-2 lg:hidden">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => setActiveIndex(index)}
+              className={`w-2 h-2 rounded-full transition-colors ${
+                activeIndex === index ? "bg-gray-500" : "bg-gray-300"
+              }`}
+            ></button>
+          ))}
+        </div>
       </div>
-
-      {/* Indicator Buttons */}
-      <div className="flex justify-center mt-4 space-x-2">
-        {slides.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => setActiveIndex(index)}
-            className={`w-3 h-3 rounded-full transition-colors ${
-              activeIndex === index ? "bg-gray-800" : "bg-gray-300"
-            }`}
-          ></button>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
